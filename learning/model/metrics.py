f43739c1343b5c36c52df1bd19f5196449644694
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2019-03-10 20:51:00 +0300

    Update docstrings in metrics

--- learning/model/metrics.py
+++ learning/model/metrics.py
@@ -38,6 +38,9 @@ def f1(y_true, y_pred):
     """F1 metric.
 
     Only computes a batch-wise average of F1-score.
+
+    Computes the F1-score, a metric for classification,
+    weighted average of the precision and recall.
     """
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
@@ -48,13 +51,17 @@ def f1(y_true, y_pred):
     return 2 * ((pre * rec) / (pre + rec + K.epsilon()))
 
 
-def auc_roc(y_true, y_pred):
-    # any tensorflow metric
-    value, update_op = tf.contrib.metrics.streaming_auc(y_pred, y_true)
+def roc_auc(y_true, y_pred):
+    """ROC AUC metric.
+
+    Only computes a batch-wise average of ROC AUC score.
+
+    For more information see https://www.tensorflow.org/api_docs/python/tf/metrics/auc
+    """
+    value, update_op = tf.contrib.metrics.streaming_auc(y_pred, y_true)  # any tensorflow metric
 
     # find all variables created for this metric
-    metric_vars = [i for i in tf.local_variables(
-    ) if 'auc_roc' in i.name.split('/')[1]]
+    metric_vars = [i for i in tf.local_variables() if 'auc_roc' in i.name.split('/')[1]]
 
     # Add metric variables to GLOBAL_VARIABLES collection.
     # They will be initialized for new session.
