75ddc22b7861c0c74d46958c9f891475ca00fc19
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2019-03-10 22:07:58 +0300

    Update augmentor

--- learning/preparation/augmentor.py
+++ learning/preparation/augmentor.py
@@ -1,21 +1,19 @@
 from functools import partial
 import numpy as np
 from PIL import Image, ImageEnhance
+import cv2
 
 
 class Augmentor:
 
-    def __init__(self, angle=(-180, 180), rl_flip=True, tb_flip=True, gamma=(0.5, 1.5), brightness=(0.5, 2),
-                 contrast=(0.5, 1.5), hue=(-0.03, 0.03), saturation=(0.5, 1.5), color_augment_count=2):
+    def __init__(self, angle=(-10, 10), rl_flip=True, brightness=(0.5, 2),
+                 contrast=(0.5, 1.5), shift=0.1, zoom=0.1):
         self.angle = angle
         self.rl_flip = rl_flip
-        self.tb_flip = tb_flip
-        self.gamma = gamma
         self.brightness = brightness
         self.contrast = contrast
-        self.hue = hue
-        self.saturation = saturation
-        self.color_augment_count = color_augment_count
+        self.shift = shift
+        self.zoom = zoom
 
     def augment(self, img):
 
@@ -29,21 +27,27 @@ class Augmentor:
         if self.rl_flip:
             img = self._make_probabilistic(self._rl_flip, img=img)
 
-        color_augmentations = np.array([[self.gamma, self._adjust_gamma],
-                                        [self.brightness, self._adjust_brightness],
-                                        [self.contrast, self._adjust_contrast],
-                                        [self.hue, self._adjust_hue],
-                                        [self.saturation, self._adjust_saturation]
-                                        ])
+        if self.shiift:
+            y, x = img.shape[:2]
+            max_dx = self.shift * x
+            max_dy = self.shift * y
+            shift_x, shift_y = (
+                np.random.randint(-max_dx, max_dx),
+                np.random.randint(-max_dy, max_dy)
+            )
+            img = self._make_probabilistic(partial(self._shift, shift_x=shift_x, shift_y=shift_y), img=img)
 
-        color_augment_logs = []
-        for _ in range(self.color_augment_count):
-            augment_index = np.random.randint(5)
-            if augment_index not in color_augment_logs:
-                color_augment_logs.append(augment_index)
-                augment_range, augment_func = color_augmentations[augment_index]
-                value = self._random(augment_range[0], augment_range[1])
-                img = self._make_probabilistic(partial(augment_func, value=value), img=img)
+        if self.zoom:
+            zoom_value = np.random.randint(-100, 100) * self.zoom / 100
+            img = self._make_probabilistic(partial(self._zoom, value=zoom_value), img=img)
+
+        if self.brightness:
+            brightness_value = self._random(self.brightness[0], self.brightness[1])
+            img = self._make_probabilistic(partial(self._adjust_brightness, value=brightness_value), img=img)
+
+        if self.contrast:
+            contrast_value = self._random(self.contrast[0], self.contrast[1])
+            img = self._make_probabilistic(partial(self._adjust_contrast, value=contrast_value), img=img)
 
         return img
 
@@ -70,22 +74,7 @@ class Augmentor:
 
         return img
 
-    def _tb_flip(self, img):
-        img = Image.fromarray(img, 'RGB')
-        img = img.transpose(Image.FLIP_TOP_BOTTOM)
-        img = np.asarray(img)
-
-        return img
-
-    def _shift(self, img, x_percent, y_percent):
-        y, x = img.shape[:2]
-        max_dx = x_percent * x
-        max_dy = y_percent * y
-        shift_x, shift_y = (
-            np.random.randint(-max_dx, max_dx),
-            np.random.randint(-max_dy, max_dy)
-        )
-
+    def _shift(self, img, shift_x, shift_y):
         if shift_x > 0:
             img[:, shift_x:] = img[:, :-shift_x]
             img[:, :shift_x] = 0
@@ -102,17 +91,16 @@ class Augmentor:
 
         return img
 
-    def _adjust_gamma(self, img, value):
-        prepared_img = Image.fromarray(img, 'RGB')
-
-        gain = 1
-        input_mode = prepared_img.mode
-        gamma_map = [255 * gain * pow(ele / 255., value) for ele in range(256)] * 3
-        prepared_img = prepared_img.point(gamma_map)
-        prepared_img = prepared_img.convert(input_mode)
-
-        prepared_img = np.asarray(prepared_img)
-        img = np.dstack([prepared_img, img])
+    def _zoom(self, img, value):
+        img_height, img_width, = img.shape[:2]
+        height_zoom = img_height * value // 2
+        width_zoom = img_width * value // 2
+        if value > 0:
+            img = img[height_zoom: -height_zoom, width_zoom: -width_zoom]
+        elif value < 0:
+            img = np.pad(img, ((height_zoom, height_zoom), (width_zoom, width_zoom)),
+                         'constant', constant_values=((0, 0), (0, 0)))
+            img = cv2.resize(img, (img_width, img_height))
 
         return img
 
@@ -131,34 +119,6 @@ class Augmentor:
         prepared_img = Image.fromarray(img, 'RGB')
 
         enhancer = ImageEnhance.Contrast(prepared_img)
-        prepared_img = enhancer.enhance(value)
-
-        prepared_img = np.asarray(prepared_img)
-        img = np.dstack([prepared_img, img])
-
-        return img
-
-    def _adjust_hue(self, img, value):
-        prepared_img = Image.fromarray(img, 'RGB')
-
-        if not(-0.5 <= value <= 0.5):
-            raise ValueError('hue_factor is not in [-0.5, 0.5].'.format(value))
-        input_mode = prepared_img.mode
-        h, s, v = prepared_img.convert('HSV').split()
-        np_h = np.array(h, dtype=np.uint8)
-        np_h += np.uint8(value * 255)
-        h = Image.fromarray(np_h, 'L')
-        prepared_img = Image.merge('HSV', (h, s, v)).convert(input_mode)
-
-        prepared_img = np.asarray(prepared_img)
-        img = np.dstack([prepared_img, img])
-
-        return img
-
-    def _adjust_saturation(self, img, value):
-        prepared_img = Image.fromarray(img, 'RGB')
-
-        enhancer = ImageEnhance.Color(prepared_img)
         prepared_img = enhancer.enhance(value)
 
         prepared_img = np.asarray(prepared_img)
