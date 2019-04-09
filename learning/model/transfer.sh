8f0d77bc9aeb0a42d71f46120e4ff7f80137627f
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2019-04-09 17:08:38 +0300

    Back transfer mmdnn file, and make it runnable from learning directory

---
+++ learning/model/transfer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 ./model/keras_model_resave.py
+
+model_folders="./model/saved_models/*/";
+for model_folder in $model_folders; do
+    mmconvert -sf keras -iw "$model_folder/model.h5" -in "$model_folder/model.json" -df pytorch -om "$model_folder/model.pth";
+    echo "$model_folder was convert to PyTorch"; done
+
